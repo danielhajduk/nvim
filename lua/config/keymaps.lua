4ee0ea70f16bc6e82dd5ad1dba7537d61b89bcfe
Daniel Hajduk <dhajduk176@gmail.com>
2023-10-13 20:54:59 +0200

    Delete conflicting keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -31,3 +31,7 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
+
+-- Delete LazyVim keymaps
+vim.keymap.del("n", "s")
+vim.keymap.del("n", "S")
